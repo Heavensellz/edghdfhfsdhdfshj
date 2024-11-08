a337e2880d73682c99be93c2b00212da824bf8fb
Heavensellz <heavensellz@gmail.com>
2024-11-08 20:52:05 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_ENDPOINT = 'https://discord.com/api/v10';
-export const WEB_URL = process.env.WEB_URL ?? 'http://localhost:3000';
-export const BOT_TOKEN = process.env['BOT_TOKEN'];
+export const WEB_URL = process.env.WEB_URL ?? 'https://wavewebsite.vercel.app/';
+export const BOT_TOKEN = process.env['MTI2NzA0OTE0MjAyMzA5ODM5OA.G0GVtm.xbdUewMgxcqlOQxW1PlqMZPjh300-j5RrpecC0'];
