615cbe95413d5815eaa34618250218a398a45f3c
Heavensellz <heavensellz@gmail.com>
2024-11-08 22:50:40 +0200

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,3 @@
 export const API_ENDPOINT = 'https://discord.com/api/v10';
-export const WEB_URL = process.env.WEB_URL ?? 'https://wavewebsite.vercel.app/';
-export const BOT_TOKEN = process.env['MTI2NzA0OTE0MjAyMzA5ODM5OA.G0GVtm.xbdUewMgxcqlOQxW1PlqMZPjh300-j5RrpecC0'];
+export const WEB_URL = process.env.WEB_URL ?? 'https://wave-rho-gold.vercel.app';
+export const BOT_TOKEN = process.env['MTI2NzA0OTE0MjAyMzA5ODM5OA.GJWM_s.tkVfsJj9V9Ep4qkz6CMPvDef-Gli2BAEtytMd4'];
